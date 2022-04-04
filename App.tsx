067f68f2f468ae55d1eb1b2ff0aaf4b9bf230882
Javier MC <mtzcornejo66@gmail.com>
2022-04-04 14:54:17 -0500

    Added some changes

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet } from 'react-native'
+import { SafeAreaProvider } from 'react-native-safe-area-context'
+
+//Screns
+import { SplashScreen } from './src/screens'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaProvider>
+      <SplashScreen />
+    </SafeAreaProvider>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -15,6 +18,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    justifyContent: 'center'
+  }
+})
---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,7 @@
+import { StyleSheet, Text } from 'react-native'
+
+const SplashScreen = () => {
+  return <Text>SplashScreen</Text>
+}
+
+export default SplashScreen
